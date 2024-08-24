3233047d73cd97aef13d841ce2ca3e6c88234427
Zoltan Toma <zoltantoma87@gmail.com>
2024-08-24 19:17:50 +0200

    First part

---
+++ README.md
@@ -0,0 +1,7 @@
+# GoBank
+
+This is just a practice project. :)
+
+Based on [Complete JSON API project in Golang (JWT, Postgres, and Docker)](https://www.youtube.com/watch?v=pwZuNmAzaH8&list=PL0xRBLFXXsP6nudFDqMXzrvQCZrxSOm-2) by [Anthony GG](https://www.youtube.com/@anthonygg_)
+
+PS: Thank you! :) I needed some finger work. :)
---
+++ api.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func WriteJson(w http.ResponseWriter, status int, v any) error {
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(status)
+	return json.NewEncoder(w).Encode(v)
+}
+
+type apiFunc func(http.ResponseWriter, *http.Request) error
+
+type ApiError struct {
+	Error string
+}
+
+func makeHttpHandleFunc(f apiFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if err := f(w, r); err != nil {
+			WriteJson(w, http.StatusBadRequest, ApiError{Error: err.Error()})
+		}
+	}
+}
+
+type ApiServer struct {
+	listenAddr string
+}
+
+func NewApiServer(listenAddr string) *ApiServer {
+	return &ApiServer{
+		listenAddr: listenAddr,
+	}
+}
+
+func (s *ApiServer) Run() {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/account", makeHttpHandleFunc(s.handleAccount))
+	router.HandleFunc("/account/{id}", makeHttpHandleFunc(s.handleGetAccount))
+
+	log.Println("Json Api listen on address ", s.listenAddr)
+	http.ListenAndServe(s.listenAddr, router)
+}
+
+func (s *ApiServer) handleAccount(w http.ResponseWriter, r *http.Request) error {
+	// if r.Method == http.MethodGet {
+	// 	return s.handleGetAccount(w, r)
+	// }
+	if r.Method == http.MethodPost {
+		return s.handleCreateAccount(w, r)
+	}
+	if r.Method == http.MethodDelete {
+		return s.handleDeleteAccount(w, r)
+	}
+	return fmt.Errorf("method not allowed %s", r.Method)
+}
+
+func (s *ApiServer) handleGetAccount(w http.ResponseWriter, r *http.Request) error {
+	if r.Method != http.MethodGet {
+		return fmt.Errorf("method not allowed")
+	}
+	id := mux.Vars(r)["id"]
+	fmt.Println("Requested account: ", id)
+	account := NewAccount("leeshan", "zoli")
+	return WriteJson(w, http.StatusOK, account)
+}
+
+func (s *ApiServer) handleCreateAccount(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
+
+func (s *ApiServer) handleDeleteAccount(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
+
+func (s *ApiServer) handleTransfare(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
